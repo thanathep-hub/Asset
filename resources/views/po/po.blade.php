ed7a5e34d0045bdc0cc9c24b8206df88f0b36a55
Honhack <oohtshon@gmail.com>
2024-06-07 11:36:15 +0700

    image count 20

--- resources/views/po/po.blade.php
+++ resources/views/po/po.blade.php
@@ -748,7 +748,7 @@
                     return false;
                 }
             }
-            var image = [1, 2, 3];
+            var image = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
             var imagList = [];
             var counter = 0;
             var promises = image.map(async (element) => {
